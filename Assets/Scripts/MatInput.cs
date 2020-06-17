0afed925acf88952f912f4b99c3194b5fb6733b9
Ginosik <luis.ega@hotmail.com>
2020-06-17 20:17:57 -0300

    Initial commit

---
+++ Assets/Scripts/MatInput.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MatInput : MonoBehaviour
+{
+
+    private int number1, number2, result;
+    public Text firstTerm, secondTerm, sign, input;
+    private string message;
+    bool enter = false;
+
+    void Start()
+    {
+        number1 = (int)Random.Range(1.0f, 100.0f);
+        firstTerm.text = number1.ToString();
+        number2 = (int)Random.Range(1.0f, 100.0f);
+        secondTerm.text = number2.ToString();
+        result = number1 + number2;
+        input.text = input.GetComponent<Text>().text;
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            enter = true;
+        }
+        
+        if (enter)
+        {
+            if (int.Parse(input.text) == result)
+            {
+                message = "Correct";
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            else
+            {
+                message = "Wrong";
+            }
+            enter = false;
+            Debug.Log(message);
+        }
+    }
+
+    public void Enter()
+    {
+        enter = true;
+    }
+
+}
